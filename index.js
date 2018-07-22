914b9f59f32b9ee91b3db65fd1621dd8982cfac3
Heather Miller <heather.miller114@gmail.com>
2018-07-22 03:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {
   flour: '2 cups'
 };
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
   return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {

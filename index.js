93939b390408cac8ecfad706fdfa065316fef650
Heather Miller <heather.miller114@gmail.com>
2018-07-22 03:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object.key = 'value';
   return object;
 }
 

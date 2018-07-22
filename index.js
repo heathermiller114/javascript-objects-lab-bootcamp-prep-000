34cce3c096465e83fb6d5c933dec003bb1ba41a8
Heather Miller <heather.miller114@gmail.com>
2018-07-22 03:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {
   flour: '2 cups'
 };
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign( {}, object,)
+  Object.assign( {}, object, {key})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
